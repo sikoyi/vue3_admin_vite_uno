f938c241ed03a5898d8ec6f20264f73b341a272b
袁升 <yuansheng@email.com>
2023-11-30 00:00:54 +0800

    update

---
+++ src/layout/components/logo/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div v-if="setting.logoHidden" class="logo-container">
+		<img :src="setting.logoPath" alt="" />
+		<p>{{ setting.title }}</p>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import setting from '@/setting';
+</script>
+
+<style lang="scss" scoped>
+.logo-container {
+	width: 100%;
+	height: var(--base-menu-logo-height);
+	color: white;
+	display: flex;
+	align-items: center;
+    margin-left: 20px;
+	font-size: var(--base-logo-title-font-size);
+	img {
+		width: 40px;
+		height: 40px;
+	}
+}
+</style>
--- src/layout/components/sideNavBar/index.vue
+++ src/layout/components/sideNavBar/index.vue
@@ -1,9 +1,15 @@
 <template>
-	<el-aside class="w-[var(--base-menu-width)] h-full bg-[var(--base-menu-background)]">
-		<el-dropdown></el-dropdown>
+	<el-aside class="w-[var(--base-menu-width)] bg-[var(--base-menu-background)]">
+		<Logo></Logo>
+		<Menu></Menu>
 	</el-aside>
 </template>
 
 <script setup lang="ts">
+import Logo from '../logo/index.vue';
+import Menu from '../menu/index.vue';
+
 defineOptions({ name: 'SideNavBar' });
 </script>
+
+<style lang="sass" scoped></style>

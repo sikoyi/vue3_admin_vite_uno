91180354ad8354f4167e1c3ce5e8e184cca9d0c3
yuansheng <yuansheng@email.com>
2024-01-21 18:28:21 +0800

    update

--- src/layout/components/tab-bar/setting/index.vue
+++ src/layout/components/tab-bar/setting/index.vue
@@ -29,7 +29,6 @@ import useUserStore from '@/store/modules/user';
 
 let settingStore = useLayOutSettingStore();
 const userStore = useUserStore();
-console.log(userStore)
 const $router = useRouter();
 const $route = useRoute();
 
